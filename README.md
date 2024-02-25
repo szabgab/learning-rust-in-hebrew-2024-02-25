a14cacf7cd9c4b50093e830f8e975eb79712c79d
Gabor Szabo <gabor@szabgab.com>
2024-02-25 12:30:33 +0200

    error handling

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rust
+
+
+https://www.rust-lang.org/
+https://doc.rust-lang.org/stable/clippy/usage.html
+https://github.com/rust-lang/rust-clippy
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 fn main() {
-    let name = "Foo Bar";
-    println!("Hello, {name}!");
+    let input = "420x";
+    //let number: i32 = input.parse().unwrap();
+
+    let number: i32 = match input.parse() {
+        Ok(val) => val,
+        //Err(err) => panic!("not good {err}"),
+        Err(err) => {
+            eprintln!("not good {err}");
+            std::process::exit(1);
+        },
+    };
+
+    dbg!(number);
+    // let name = "Foo Bar";
+    // println!("Hello, {name}!");
 }
