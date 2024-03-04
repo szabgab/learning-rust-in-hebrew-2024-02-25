5200466b894539ad061327cf6378a2491efdbf96
Gabor Szabo <gabor@szabgab.com>
2024-03-04 10:55:46 +0200

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,20 @@
 fn main() {
+    let numbers = [23, 8, 4, 7, 7, 4, 19];
+    let number = 4;
+
+    let counter = count_number(numbers, number);
+    println!("Number of time {number} appears in {numbers:?} is {counter}");
 }
+
+fn count_number(numbers: [i32; 7], number: i32) -> i32 {
+    let mut counter = 0;
+    for value in numbers {
+        //println!("{value}");
+        if value == number {
+            //counter = counter + 1;
+            counter += 1;
+        }
+    }
+    counter
+}
+
