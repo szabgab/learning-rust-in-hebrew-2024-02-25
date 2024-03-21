6f67f5a8b3aa75965169678c0fde51b1e911b9df
Gabor Szabo <gabor@szabgab.com>
2024-03-21 18:40:25 +0200

    add 18

--- README.md
+++ README.md
@@ -935,3 +935,49 @@ $ cargo run -q
 39 50 0x7ffd34b4de00 0x63d04e94aba0  'ab123456x123456789012345678901234567890'
 
 ```
+
+# 18. `&str` is a string slice in the memory
+
+* [video 18]()
+* [page](https://he.code-maven.com/rust-course-18)
+
+
+```rust
+macro_rules! prt {
+    ($var: expr) => {
+        println!("{:2} {:2} {:p} {:<15?} '{}'", $var.len(), $var.capacity(), &$var, $var.as_ptr(), $var);
+    };
+}
+
+macro_rules! prt_str {
+    ($var: expr) => {
+        println!("{:2}    {:p} {:<15?} '{}'", $var.len(), &$var, $var.as_ptr(), $var);
+    };
+}
+
+fn main() {
+    let text = "Hello foo, how are you";
+    prt_str!(text);
+
+    //let text = String::from(text);
+    //let text = text.to_string();
+    let text = text.to_owned();
+    prt!(text);
+
+    let name = &text[6..=8];
+    prt_str!(name);
+
+    let fname = String::from("bar");
+    prt!(fname);
+
+    let content = std::fs::read_to_string("names.txt").unwrap();
+    prt!(content);
+    let words = content.split('\n').collect::<Vec<&str>>();
+    //prt!(words);
+    prt_str!(words[0]);
+    prt_str!(words[1]);
+    prt_str!(words[2]);
+    prt_str!(words[3]);
+    println!("{:2} {:2} {:p} {:<15?}", words.len(), words.capacity(), &words, words.as_ptr());
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,36 @@ macro_rules! prt {
     };
 }
 
-// ptr
-// len
-// capacity
+macro_rules! prt_str {
+    ($var: expr) => {
+        println!("{:2}    {:p} {:<15?} '{}'", $var.len(), &$var, $var.as_ptr(), $var);
+    };
+}
+
 fn main() {
-    //let mut text = String::new();    
-    let mut text = String::with_capacity(50);    
+    let text = "Hello foo, how are you";
+    prt_str!(text);
+
+    //let text = String::from(text);
+    //let text = text.to_string();
+    let text = text.to_owned();
     prt!(text);
 
-    text.push('a');
-    prt!(text);
+    let name = &text[6..=8];
+    prt_str!(name);
 
-    let name = String::from("foo");
-    prt!(name);
+    let fname = String::from("bar");
+    prt!(fname);
 
-    text.push('b');
-    prt!(text);
-
-    text.push_str("123456");
-    prt!(text);
-
-    text.push('x');
-    prt!(text);
-
-    text.push_str("123456789012345678901234567890");
-    prt!(text);
+    let content = std::fs::read_to_string("names.txt").unwrap();
+    prt!(content);
+    let words = content.split('\n').collect::<Vec<&str>>();
+    //prt!(words);
+    prt_str!(words[0]);
+    prt_str!(words[1]);
+    prt_str!(words[2]);
+    prt_str!(words[3]);
+    println!("{:2} {:2} {:p} {:<15?}", words.len(), words.capacity(), &words, words.as_ptr());
 
 
 }
